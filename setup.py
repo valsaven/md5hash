ae97ced3c088f5350d20e1d8888728bdd0e626fb
Val Saven <val.saven@gmail.com>
2020-07-30 00:03:11 +0300

    Add long description

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 
+import os.path
 from setuptools import setup, find_packages
 
 setup(
     name='md5hash',
-    version='0.0.4',
+    version='0.0.5',
     packages=find_packages(),
     requires=['Python (>= 3.5)'],
     description='Fast and simple md5 hash generator for files and directories',
+    long_description=(open('README.rst').read() if os.path.exists('README.rst') else ''),
     author='Val Saven',
     author_email='val.saven@gmail.com',
     url='https://github.com/valsaven/md5hash/',
